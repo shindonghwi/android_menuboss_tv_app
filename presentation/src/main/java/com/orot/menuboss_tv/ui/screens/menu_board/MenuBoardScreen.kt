a4c0a7482d3e12cac21143f485aad133f1e09fe4
shindonghwi <ehdgnl8940@naver.com>
2023-10-12 08:52:59 +0900

    feat: 구독만료, 컨텐츠 없음 화면 추가

--- presentation/src/main/java/com/orot/menuboss_tv/ui/screens/menu_board/MenuBoardScreen.kt
+++ presentation/src/main/java/com/orot/menuboss_tv/ui/screens/menu_board/MenuBoardScreen.kt
@@ -2,13 +2,19 @@ package com.orot.menuboss_tv.ui.screens.menu_board
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.tv.material3.Text
 import com.orot.menuboss_tv.ui.model.UiState
 import com.orot.menuboss_tv.ui.navigations.LocalMainViewModel
@@ -17,6 +23,11 @@ import com.orot.menuboss_tv.ui.navigations.RouteScreen
 import com.orot.menuboss_tv.ui.screens.menu_board.widget.PlaylistSlider
 import com.orot.menuboss_tv.ui.screens.menu_board.widget.ScheduleSlider
 import com.orot.menuboss_tv.ui.screens.reload.ReloadScreen
+import com.orot.menuboss_tv.ui.source_pack.IconPack
+import com.orot.menuboss_tv.ui.source_pack.iconpack.Logo
+import com.orot.menuboss_tv.ui.theme.AdjustedBoldText
+import com.orot.menuboss_tv.ui.theme.AdjustedMediumText
+import com.orot.menuboss_tv.utils.adjustedDp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.URLEncoder
@@ -29,14 +40,16 @@ fun MenuBoardScreen() {
     val screenState = mainViewModel.screenState.collectAsState().value
     val deviceState = mainViewModel.deviceState.collectAsState().value
 
-    LaunchedEffect(key1 = screenState){
+    LaunchedEffect(key1 = screenState) {
         when (screenState) {
             is UiState.Success -> {
                 val screenData = screenState.data
                 if (screenData?.isDeleted == true) {
                     mainViewModel.requestGetDeviceInfo()
                 }
-            }else->{}
+            }
+
+            else -> {}
         }
     }
 
@@ -71,16 +84,16 @@ fun MenuBoardScreen() {
             }
 
             is UiState.Error -> {
-                ErrorBox("Error Retry")
+                ReloadScreen()
             }
 
             is UiState.Success -> {
                 val screenData = item.data
 
                 if (screenData?.isDeleted == true) {
-                    Text("Screen Deleted")
+                    ReloadScreen()
                 } else if (screenData?.isExpired == false) {
-                    Text("Screen Expired")
+                    ExpiredScreen(modifier = Modifier.fillMaxSize())
                 } else {
                     when (screenData?.isPlaylist) {
                         true -> {
@@ -91,7 +104,7 @@ fun MenuBoardScreen() {
                             screenData.scheduleModel?.let { ScheduleSlider(model = it) }
                         }
 
-                        null -> Text("Empty Contents")
+                        null -> EmptyContentScreen(modifier = Modifier.fillMaxSize())
                     }
                 }
             }
@@ -100,12 +113,62 @@ fun MenuBoardScreen() {
 }
 
 @Composable
-private fun ErrorBox(message: String) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+private fun EmptyContentScreen(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = message)
+        Image(
+            modifier = modifier.size(
+                width = adjustedDp(150.dp),
+                height = adjustedDp(75.dp),
+            ), imageVector = IconPack.Logo, contentDescription = ""
+        )
+        AdjustedBoldText(
+            modifier = Modifier.padding(top = adjustedDp(40.dp)),
+            text = "No Content to display",
+            fontSize = adjustedDp(24.dp),
+        )
+
+        AdjustedMediumText(
+            modifier = Modifier.padding(top = adjustedDp(12.dp)),
+            text = "MenuBoss Please register your schedule and playlist through the web or mobile",
+            fontSize = adjustedDp(16.dp)
+        )
     }
 }
 
+@Composable
+private fun ExpiredScreen(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = modifier.size(
+                width = adjustedDp(150.dp),
+                height = adjustedDp(75.dp),
+            ), imageVector = IconPack.Logo, contentDescription = ""
+        )
+
+        AdjustedBoldText(
+            modifier = Modifier.padding(top = adjustedDp(40.dp)),
+            text = "Your subscription has expired",
+            fontSize = adjustedDp(24.dp),
+        )
+
+        AdjustedMediumText(
+            modifier = Modifier.padding(top = adjustedDp(12.dp)),
+            text = "You cannot use it because your subscription period has expired.",
+            fontSize = adjustedDp(16.dp)
+        )
+
+        AdjustedMediumText(
+            modifier = Modifier.padding(top = adjustedDp(4.dp)),
+            text = "You can use it again by subscribing to the service",
+            fontSize = adjustedDp(16.dp)
+        )
+    }
+}
