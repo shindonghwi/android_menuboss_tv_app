5368fd3d515df1349f6635904ef8f446e9b8dea4
shindonghwi <ehdgnl8940@naver.com>
2023-10-15 12:55:33 +0900

    fix: view model 분리

--- presentation/src/main/java/com/orot/menuboss_tv/ui/screens/splash/SplashScreen.kt
+++ presentation/src/main/java/com/orot/menuboss_tv/ui/screens/splash/SplashScreen.kt
@@ -9,49 +9,49 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import app.rive.runtime.kotlin.core.Loop
 import com.orot.menuboss_tv.presentation.R
 import com.orot.menuboss_tv.ui.components.RiveAnimation
 import com.orot.menuboss_tv.ui.model.UiState
-import com.orot.menuboss_tv.ui.navigations.LocalMainViewModel
 import com.orot.menuboss_tv.ui.navigations.LocalNavController
 import com.orot.menuboss_tv.ui.navigations.RouteScreen
 import com.orot.menuboss_tv.ui.theme.colorBackground
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.net.URLEncoder
 
 @Composable
 fun SplashScreen(
+    uuid: String,
+    splashViewModel: SplashViewModel = hiltViewModel()
 ) {
     val navController = LocalNavController.current
-    val mainViewModel = LocalMainViewModel.current
 
-    val deviceState = mainViewModel.deviceState.collectAsState().value
-    val shouldNavigateToAuth = mainViewModel.navigateToAuthScreen.collectAsState().value
-    val shouldNavigateToMenuBoard = mainViewModel.navigateToMenuBoardScreen.collectAsState().value
+    val deviceState = splashViewModel.deviceState.collectAsState().value
+    val doAuthScreenActionState = splashViewModel.navigateToAuthState.collectAsState().value
+    val doMenuScreenActionState = splashViewModel.navigateToMenuState.collectAsState().value
 
     /**
-     * @feature: 스플래시 화면에서 상태를 초기화하고, 최초로 디바이스 정보를 가져옵니다.
-     * @author: 2023/10/02 6:07 PM donghwishin
-     * @description {
-     *   여기서 디바이스 정보를 가져오는 이유는,
-     *   디바이스의 상태가 연결되었는지를 판단한다.
-     * }
+     * @feature: 디바이스 정보 요청
+     * @author: 2023/10/15 12:52 PM donghwishin
      */
+    DisposableEffect(key1 = Unit, effect = {
+        splashViewModel.run {
+            // 디바이스 정보 요청
+            CoroutineScope(Dispatchers.Main).launch { requestGetDeviceInfo(uuid = uuid) }
 
-    LaunchedEffect(key1 = Unit, block = {
-        mainViewModel.requestGetDeviceInfo()
+            // 뷰 모델 상태 초기화
+            onDispose { initState() }
+        }
     })
 
     /**
      * @feature: 인증화면으로 이동하는 기능
      * @author: 2023/10/12 1:06 PM donghwishin
      */
-    LaunchedEffect(shouldNavigateToAuth) {
-        if (shouldNavigateToAuth) {
+    LaunchedEffect(doAuthScreenActionState) {
+        if (doAuthScreenActionState) {
             navController.navigate(RouteScreen.AuthScreen.route) {
                 popUpTo(navController.graph.startDestinationId) {
                     inclusive = true
@@ -64,8 +64,8 @@ fun SplashScreen(
      * @feature: 메뉴판 화면으로 이동하는 기능
      * @author: 2023/10/12 1:06 PM donghwishin
      */
-    LaunchedEffect(shouldNavigateToMenuBoard) {
-        if (shouldNavigateToMenuBoard) {
+    LaunchedEffect(doMenuScreenActionState) {
+        if (doMenuScreenActionState) {
             navController.navigate(RouteScreen.MenuBoardScreen.route) {
                 popUpTo(navController.graph.startDestinationId) {
                     inclusive = true
@@ -75,43 +75,28 @@ fun SplashScreen(
     }
 
     /**
-     * @feature: 스플래시 화면에서 디바이스 정보를 가져온 후, 상태에 따라서 다음 화면으로 이동합니다.
-     *
-     * @author: 2023/10/02 6:08 PM donghwishin
+     * @feature: 디바이스 정보 응답 관찰
+     * @author: 2023/10/15 12:52 PM donghwishin
      *
      * @description{
-     *   1. 디바이스가 연결이 안되어있는 경우
-     *       * Connect Stream을 구독합니다.
-     *       * 조회한 디바이스 정보로 부터 code, qrUrl 정보를 가지고 AuthScreen 으로 이동.
-     *       * AuthScreen 에서 디바이스 등록이 된 후에 Connect Stream 을 구독하고, MenuBoardScreen 으로 이동.
      *
-     *   2. 디바이스가 연결 되어있는 경우
-     *       * Content Stream을 구독합니다.
-     *       * 조회한 디바이스 정보로 부터 accessToken 정보를 가지고 MenuBoardScreen 으로 이동.
+     *  1. 디바이스 정보가 정상적으로 응답되면,
+     *      1-1. 디바이스 상태가 Unlinked이면, 인증화면으로 이동합니다.
+     *      1-2. 디바이스 상태가 Linked이면, 메뉴판 화면으로 이동합니다.
+     *
+     *  2. 디바이스 정보가 정상적으로 응답되지 않으면, 디바이스 정보를 다시 요청합니다.
      * }
      */
-    DisposableEffect(key1 = deviceState, effect = {
-        CoroutineScope(Dispatchers.Main).launch {
+    LaunchedEffect(key1 = Unit, block = {
+        splashViewModel.run {
             if (deviceState is UiState.Success) {
                 if (deviceState.data?.status == "Unlinked") {
-                    mainViewModel.run {
-                        subscribeConnectStream()
-                        triggerAuthState(true)
-                    }
+                    triggerAuthState(true)
                 } else if (deviceState.data?.status == "Linked") {
-                    mainViewModel.run {
-                        subscribeContentStream(deviceState.data.property?.accessToken.toString())
-                        triggerMenuBoardState(true)
-                    }
+                    triggerMenuState(true)
                 }
             } else if (deviceState is UiState.Error) {
-                mainViewModel.requestGetDeviceInfo(delayTime = 3000)
-            }
-        }
-        onDispose {
-            mainViewModel.run {
-                triggerAuthState(false)
-                triggerMenuBoardState(false)
+                requestGetDeviceInfo(uuid = uuid)
             }
         }
     })
--- presentation/src/main/java/com/orot/menuboss_tv/ui/screens/splash/SplashViewModel.kt
+++ presentation/src/main/java/com/orot/menuboss_tv/ui/screens/splash/SplashViewModel.kt
@@ -8,7 +8,6 @@ import com.orot.menuboss_tv.domain.entities.Resource
 import com.orot.menuboss_tv.domain.usecases.GetDeviceUseCase
 import com.orot.menuboss_tv.firebase.FirebaseAnalyticsUtil
 import com.orot.menuboss_tv.ui.model.UiState
-import com.orot.menuboss_tv.ui.screens.auth.AuthViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -28,53 +27,58 @@ class SplashViewModel @Inject constructor(
         private const val TAG = "SplashViewModel"
     }
 
-    private val _navigateToAuthScreen = MutableStateFlow(false)
-    val navigateToAuthScreen: StateFlow<Boolean> = _navigateToAuthScreen
+    /**
+     * @feature: 인증화면으로 이동하는 기능
+     * @author: 2023/10/15 12:40 PM donghwishin
+     */
+    private val _navigateToAuthState = MutableStateFlow(false)
+    val navigateToAuthState: StateFlow<Boolean> get() = _navigateToAuthState
+    fun triggerAuthState(flag: Boolean) = run { _navigateToAuthState.value = flag }
 
-    fun triggerNavigateToAuthScreen() {
-        _navigateToAuthScreen.value = true
-    }
-
-    private val _navigateToMenuBoardScreen = MutableStateFlow(false)
-    val navigateToMenuBoardScreen: StateFlow<Boolean> = _navigateToMenuBoardScreen
-
-    fun triggerNavigateToMenuBoardScreen() {
-        _navigateToMenuBoardScreen.value = true
-    }
+    /**
+     * @feature: 메뉴화면으로 이동하는 기능
+     * @author: 2023/10/15 12:41 PM donghwishin
+     */
+    private val _navigateToMenuState = MutableStateFlow(false)
+    val navigateToMenuState: StateFlow<Boolean> get() = _navigateToMenuState
+    fun triggerMenuState(flag: Boolean) = run { _navigateToMenuState.value = flag }
 
     /**
      * @feature: 디바이스 정보를 관리합니다.
      * @author: 2023/10/03 11:38 AM donghwishin
      */
-    val deviceState = MutableStateFlow<UiState<DeviceModel>>(UiState.Idle)
+    private val _deviceState = MutableStateFlow<UiState<DeviceModel>>(UiState.Idle)
+    val deviceState: StateFlow<UiState<DeviceModel>> get() = _deviceState
 
     /**
      * @feature: 디바이스 정보를 조회합니다.
      * @author: 2023/10/03 11:39 AM donghwishin
      */
     suspend fun requestGetDeviceInfo(uuid: String) {
-        Log.w(TAG, "SplashViewModel requestGetDeviceInfo: $uuid", )
+        Log.w(TAG, "requestGetDeviceInfo: $uuid")
         viewModelScope.launch {
             delay(3000)
-            Log.w(TAG, "Splash requestGetDeviceInfo: $uuid")
+            Log.w(TAG, "requestGetDeviceInfo: $uuid")
             firebaseAnalyticsUtil.recordEvent(
                 FirebaseAnalyticsUtil.Event.GET_DEVICE_INFO,
                 hashMapOf("uuid" to uuid)
             )
 
             getDeviceUseCase(uuid).onEach {
+                Log.w(TAG, "requestGetDeviceInfo - response: $it", )
                 when (it) {
-                    is Resource.Loading -> deviceState.emit(UiState.Loading)
-                    is Resource.Error -> deviceState.emit(UiState.Error(it.message.toString()))
-                    is Resource.Success -> deviceState.emit(UiState.Success(data = it.data))
+                    is Resource.Loading -> _deviceState.emit(UiState.Loading)
+                    is Resource.Error -> _deviceState.emit(UiState.Error(it.message.toString()))
+                    is Resource.Success -> _deviceState.emit(UiState.Success(data = it.data))
                 }
             }.launchIn(this)
         }
     }
 
-    fun resetStates() {
-        _navigateToAuthScreen.value = false
-        _navigateToMenuBoardScreen.value = false
-        deviceState.value = UiState.Idle
+    fun initState(){
+        Log.w(TAG, "initState", )
+        triggerAuthState(false)
+        triggerMenuState(false)
+        _deviceState.value = UiState.Idle
     }
 }
