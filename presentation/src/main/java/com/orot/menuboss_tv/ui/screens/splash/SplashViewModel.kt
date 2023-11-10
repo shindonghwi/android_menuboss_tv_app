5e72bb03acf3c7d9f1fe24ee857006215c030fd8
shindonghwi <ehdgnl8940@naver.com>
2023-11-10 23:58:38 +0900

    fix: 재시도 지수 백오프 시간 증가율 수정

--- presentation/src/main/java/com/orot/menuboss_tv/ui/screens/splash/SplashViewModel.kt
+++ presentation/src/main/java/com/orot/menuboss_tv/ui/screens/splash/SplashViewModel.kt
@@ -64,14 +64,14 @@ class SplashViewModel @Inject constructor(
                         is Resource.Success -> handleSuccess(resource.data)
                     }
                 }
-                attempt++  // 재시도 횟수 증가
+                attempt++
             }
         }
     }
 
     private fun calculateDelay(attempt: Int): Long {
         val maxDelay = 30000L  // 최대 지연 시간 (예: 30초)
-        val delay = (2.0.pow(attempt.toDouble()) * 1000L).toLong() // 지수 백오프
+        val delay = (1.3.pow(attempt.toDouble()) * 1000L).toLong() // 지수 백오프
         return min(delay, maxDelay)
     }
 
