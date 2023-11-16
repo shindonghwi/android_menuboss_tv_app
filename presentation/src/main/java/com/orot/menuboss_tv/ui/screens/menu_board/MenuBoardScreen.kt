791afca8bcb9baf77ddaf23b082fdc3654433ef3
shindonghwi <ehdgnl8940@naver.com>
2023-11-16 17:31:43 +0900

    style: 컨텐츠 없음, 구독 만료 시 디자인 변경

--- presentation/src/main/java/com/orot/menuboss_tv/ui/screens/menu_board/MenuBoardScreen.kt
+++ presentation/src/main/java/com/orot/menuboss_tv/ui/screens/menu_board/MenuBoardScreen.kt
@@ -36,6 +36,7 @@ import com.orot.menuboss_tv.ui.source_pack.IconPack
 import com.orot.menuboss_tv.ui.source_pack.iconpack.Logo
 import com.orot.menuboss_tv.ui.theme.AdjustedBoldText
 import com.orot.menuboss_tv.ui.theme.AdjustedMediumText
+import com.orot.menuboss_tv.ui.theme.AdjustedSemiBoldText
 import com.orot.menuboss_tv.ui.theme.colorBackground
 import com.orot.menuboss_tv.utils.adjustedDp
 import java.util.UUID
@@ -162,22 +163,15 @@ private fun EmptyContentScreen(modifier: Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            modifier = Modifier.size(
-                width = adjustedDp(150.dp),
-                height = adjustedDp(75.dp),
-            ), imageVector = IconPack.Logo, contentDescription = ""
-        )
         AdjustedBoldText(
-            modifier = Modifier.padding(top = adjustedDp(40.dp)),
-            text = "No Content to display",
-            fontSize = adjustedDp(24.dp),
+            text = "No Content to display!",
+            fontSize = adjustedDp(48.dp),
         )
 
         AdjustedMediumText(
-            modifier = Modifier.padding(top = adjustedDp(12.dp)),
-            text = "MenuBoss Please register your schedule and playlist through the web or mobile",
-            fontSize = adjustedDp(16.dp)
+            modifier = Modifier.padding(top = adjustedDp(24.dp)),
+            text = "MenuBoss Please register your schedule and\nplaylist through the web or mobile",
+            fontSize = adjustedDp(20.dp)
         )
     }
 }
@@ -189,29 +183,21 @@ private fun ExpiredScreen(modifier: Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            modifier = Modifier.size(
-                width = adjustedDp(150.dp),
-                height = adjustedDp(75.dp),
-            ), imageVector = IconPack.Logo, contentDescription = ""
-        )
-
         AdjustedBoldText(
-            modifier = Modifier.padding(top = adjustedDp(40.dp)),
             text = "Your subscription has expired",
-            fontSize = adjustedDp(24.dp),
+            fontSize = adjustedDp(48.dp),
         )
 
-        AdjustedMediumText(
+        AdjustedSemiBoldText(
             modifier = Modifier.padding(top = adjustedDp(12.dp)),
-            text = "You cannot use it because your subscription period has expired.",
-            fontSize = adjustedDp(16.dp)
+            text = "You can not use it because your subscription period has expired.",
+            fontSize = adjustedDp(18.dp)
         )
 
-        AdjustedMediumText(
+        AdjustedSemiBoldText(
             modifier = Modifier.padding(top = adjustedDp(4.dp)),
             text = "You can use it again by subscribing to the service",
-            fontSize = adjustedDp(16.dp)
+            fontSize = adjustedDp(18.dp)
         )
     }
 }
