7e6b23f51a107ac679ca857384dd3a6353be5a6f
shindonghwi <ehdgnl8940@naver.com>
2023-11-29 15:31:02 +0900

    chore: version update (1.1.2 / 8)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -4,8 +4,8 @@ object AppConfig {
     const val compileSdk = 34
     const val minSdk = 21
     const val targetSdk = 34
-    const val versionCode = 7
-    const val versionName = "1.1.1"
+    const val versionCode = 8
+    const val versionName = "1.1.2"
     val javaVersion = JavaVersion.VERSION_11
     val jvmTarget = "11"
 }
