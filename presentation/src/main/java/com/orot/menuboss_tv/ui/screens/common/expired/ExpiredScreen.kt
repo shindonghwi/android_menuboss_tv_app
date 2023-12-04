ea8d9581c8c266d2ea3fd9e3aa30c732acc0a616
shindonghwi <ehdgnl8940@naver.com>
2023-12-04 18:36:23 +0900

    fix: 티비앱 디자인 변경 및 폰트 변경

---
+++ presentation/src/main/java/com/orot/menuboss_tv/ui/screens/common/expired/ExpiredScreen.kt
@@ -0,0 +1,35 @@
+package com.orot.menuboss_tv.ui.screens.common.expired
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.orot.menuboss_tv.presentation.R
+import com.orot.menuboss_tv.ui.theme.AdjustedBoldText
+import com.orot.menuboss_tv.ui.theme.AdjustedRegularText
+import com.orot.menuboss_tv.utils.adjustedDp
+
+
+@Composable
+fun ExpiredScreen(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        AdjustedBoldText(
+            text = stringResource(id = R.string.content_expired_title),
+            fontSize = adjustedDp(48.dp),
+        )
+
+        AdjustedRegularText(
+            modifier = Modifier.padding(top = adjustedDp(24.dp)),
+            text = "${stringResource(id = R.string.content_expired_description1)}\n${stringResource(id = R.string.content_expired_description2)}",
+            fontSize = adjustedDp(20.dp),
+        )
+    }
+}
--- presentation/src/main/java/com/orot/menuboss_tv/ui/theme/Color.kt
+++ presentation/src/main/java/com/orot/menuboss_tv/ui/theme/Color.kt
@@ -4,7 +4,9 @@ import androidx.compose.ui.graphics.Color
 
 val colorBackground = Color(0xFF000000)
 val colorWhite = Color(0xFFFFFFFF)
-val colorLightSkyBlue = Color(0xFF87CEEB)
+val colorLightSkyBlue = Color(0xFF39C3FF)
 val colorRed500 = Color(0xFFEB5757)
+val colorGray100 = Color(0xFFF1F1F1)
+val colorGray400 = Color(0xFFC2C2C2)
 val colorGray700 = Color(0xFF5C5C5C)
 val colorGray900 = Color(0xFF212121)
