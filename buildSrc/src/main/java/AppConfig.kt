9a2105de2ac176012d3bab25c6c5eede4a40b423
shindonghwi <ehdgnl8940@naver.com>
2023-12-19 20:30:55 +0900

    chore: 버전 (1.1.6) - 대용량 이미지 처리

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -4,8 +4,8 @@ object AppConfig {
     const val compileSdk = 34
     const val minSdk = 21
     const val targetSdk = 34
-    const val versionCode = 10
-    const val versionName = "1.1.5"
+    const val versionCode = 11
+    const val versionName = "1.1.6"
     val javaVersion = JavaVersion.VERSION_11
     val jvmTarget = "11"
 }
