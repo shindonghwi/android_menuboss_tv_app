dbc0659e34a76e997de54c776365e6903ba0c210
shindonghwi <ehdgnl8940@naver.com>
2024-01-19 12:55:33 +0900

    chore: min sdk 26으로 변경 (오레오 이상)

--- menuboss-kr/buildSrc/src/main/java/AppConfig.kt
+++ menuboss-kr/buildSrc/src/main/java/AppConfig.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object AppConfig {
     const val compileSdk = 34
-    const val minSdk = 21
+    const val minSdk = 26
     const val targetSdk = 34
     const val versionCode = 1
     const val versionName = "1.0.0"
