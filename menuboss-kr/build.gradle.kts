3d32f3e2494816e84983a457dbd1d278e2e808c2
shindonghwi <ehdgnl8940@naver.com>
2024-02-01 18:21:31 +0900

    chore: update codemagic

--- menuboss-kr/build.gradle.kts
+++ menuboss-kr/build.gradle.kts
@@ -21,3 +21,7 @@ plugins {
     id("com.google.firebase.crashlytics") version Versions.Plugins.crashlytics apply false
     id("com.google.protobuf") version Versions.Plugins.protobuf apply false
 }
+
+tasks.withType<JavaCompile> {
+    options.forkOptions.jvmArgs?.add("-Djava.version=17")
+}
